976ea99a881ac4362c1aafc42013624f5afa1b68
Sangha Lee <sangha@comcom.ai>
2020-09-18 11:24:46 +0900

    Create exec.py

---
+++ exec.py
@@ -0,0 +1,44 @@
+import predict
+import pandas as pd
+import argparse
+import dlib
+import uuid
+import os
+
+if __name__ == "__main__":
+
+    cnn_face_detector = dlib.cnn_face_detection_model_v1(
+        'dlib_models/mmod_human_face_detector.dat')
+    sp = dlib.shape_predictor(
+        'dlib_models/shape_predictor_5_face_landmarks.dat')
+
+    f_id = str(uuid.uuid4())
+    SAVE_DETECTED_AT = "detected_faces/" + f_id
+    predict.ensure_dir(SAVE_DETECTED_AT)  # 디렉토리 생성
+    nas = "detected_faces/race_Asian.jpg"
+    imgs = [nas]
+    print(nas)
+    predict.detect_face(imgs, SAVE_DETECTED_AT, cnn_face_detector, sp)
+    print("detected faces are saved at ", SAVE_DETECTED_AT)
+    predict.predidct_age_gender_race("test_outputs.csv", SAVE_DETECTED_AT)
+    print('end')
+    """
+    """
+
+    """
+    f_id = str(uuid.uuid4())
+    SAVE_DETECTED_AT="detected_faces/" + f_id
+    print(SAVE_DETECTED_AT)
+    imgs = ["test/race_Asian.jpg"]
+    """
+
+    f = open("test_outputs.csv", "r")
+    read = f.read()
+    split = read.split(",")
+    race = split[9]
+    race4 = split[10]
+    gender = split[11]
+    age = split[12]
+
+    arr = [race, race4, gender, age]
+    print(arr)
