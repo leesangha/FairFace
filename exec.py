66bd4414385fb2078fb828af41fad0f619295f37
Sangha Lee <sangha@comcom.ai>
2020-09-23 12:14:18 +0900

    Update exec.py

--- exec.py
+++ exec.py
@@ -15,15 +15,17 @@ if __name__ == "__main__":
     f_id = str(uuid.uuid4())
     SAVE_DETECTED_AT = "detected_faces/" + f_id
     predict.ensure_dir(SAVE_DETECTED_AT)  # 디렉토리 생성
-    nas = "detected_faces/race_Asian_face0.jpg"
+    nas = "detected_faces/race_Asian_face0.jpg"  # image location
     imgs = [nas]
     predict.detect_face(imgs, SAVE_DETECTED_AT, cnn_face_detector, sp)
     #print("detected faces are saved at ", SAVE_DETECTED_AT)
-    model_7= predict.make_model7()
-    model_4= predict.make_model4()
-    predict.predidct_age_gender_race("test_outputs.csv", SAVE_DETECTED_AT,model_7,model_4)
-    #print('end')
+    model_7 = predict.make_model7()
+    model_4 = predict.make_model4()
+    output = predict.predidct_age_gender_race(
+        "test_outputs.csv", SAVE_DETECTED_AT, model_7, model_4)
+    # print('end')
 
+    """
     f = open("test_outputs.csv", "r")
     read = f.read()
     split = read.split(",")
@@ -31,6 +33,6 @@ if __name__ == "__main__":
     race4 = split[10]
     gender = split[11]
     age = split[12]
-
-    arr = [race, race4, gender, age]
-    print(arr)
+    """
+    print(output)
+    # print(arr)
